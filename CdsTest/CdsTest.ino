f336bc024308f69ed55d6e0758f6d45a89903f18
DccShield <61929568+DccShield@users.noreply.github.com>
2020-05-04 16:35:58 +0900

    Add files via upload

---
+++ CdsTest/CdsTest.ino
@@ -0,0 +1,44 @@
+#include "cds.h"
+
+// 1-GND
+// 2-AIN1->A3
+// 3-AIN2->A4
+// 4-5V
+// 5-
+// 6-
+// 7-
+// 8-
+
+unsigned long PreviosTime = 0;
+char a,b;
+
+Cds CdsA0(0,2,200,200);
+Cds CdsA1(1,2,200,200);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("cds test program");
+  PreviosTime = millis();
+}
+void loop()
+{
+//  static Cds CdsA0(0,2,200,200);
+//  static Cds CdsA1(1,2,200,200);
+
+  if( (millis() - PreviosTime ) >= 100 ){  // 1000:1000msec 前回から1000ms経過したかチェック
+    PreviosTime = millis();
+    a = CdsA0.statechk(LOW);
+    b = CdsA1.statechk(LOW);
+
+//char temp[20];
+//sprintf(temp,"A0:%d , A1:%d",a,b);
+//Serial.println(temp);
+ 
+    if(a==HIGH)
+      CdsA0.Reset();
+
+      if(b==HIGH)
+      CdsA1.Reset();
+  }
+}
